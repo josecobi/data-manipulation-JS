109eb25215e73e4d61ba5dbccef5628fa89a83d4
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-21 17:35:21 -0500

    Part 2 completed, reformated output text

--- practice.js
+++ practice.js
@@ -50,24 +50,21 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
 // TO-DO Check if all numbers are divisible by 5. Cache the result in a variable.
  const areDivisibleByFive = (n1 % 5 === 0) && (n2 % 5 === 0) && (n3 % 5 === 0) && (n4 % 5 === 0);
- console.log(`The statement "${n1}, ${n2}, ${n3}, and ${n4} are divisible by 5" is  ${areDivisibleByFive}.`);
 
-
- 
 
 // TO-DO Check if the first number is larger than the last. Cache the result in a variable.
 const isN1LargerThanN4 = n1 > n4;
-console.log(`The statement "n1 is larger than n4" is ${isN1LargerThanN4}.`);
+
 // TO-DO Accomplish the following arithmetic chain:
 // TO-DO Subtract the first number from the second number.
 const substractN1FromN2 = n2 - n1;
-console.log(`When we substract n1 from n2 we get "${substractN1FromN2}".`);
+
 // TO-DO Multiply the result by the third number.
 const resultBythree = substractN1FromN2 * n3;
-console.log(`If we multiply the result above by ${n3} the result in the previous line is "${resultBythree}".`);
+
 // TO-DO Find the remainder of dividing the result by the fourth number.
 const remainder = resultBythree % n4;
-console.log(`The remainder of dividing the result in the previous line by n4 is "${remainder}".`);
+
 // TO-DO Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
 
 // I changed the comparison operator (>) for (<) to make the result of the comparison to be true so the negation operator(!) is not needed in other comparisons
@@ -75,7 +72,13 @@ const isBelow25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 
 const isValid2 = isSum50 && isTwoOdd && isBelow25 && isUnique;
 
-console.log(`The four numbers are valid according to the provided criteria: ${isValid2}.`);
+console.log(`>>>>>>>>PART 1: MATH PROBLEMS<<<<<<<<<<
+- The statement "${n1}, ${n2}, ${n3}, and ${n4} are divisible by 5" is  ${areDivisibleByFive}.
+- The statement "${n1} is larger than ${n4}" is ${isN1LargerThanN4}.
+- When we substract "${n1}" from "${n2}" we get "${substractN1FromN2}".
+- If we multiply the result above by "${n3}" the result in the previous line is "${resultBythree}".
+- The remainder of dividing the result (${resultBythree}) in the previous line by ${n4} is "${remainder}".
+- The four numbers are valid according to the provided criteria: ${isValid2}.\n`);
 
 
 
@@ -94,26 +97,46 @@ const pricePerGal = 3;
 //NUMBER OF GALLONS NEEDED FOR THE TRIP
 // Calculate the amount of gallons needed if travelling at an average of 55 mph
 const numberGallonsNeededIfTravel55Mph = totalDistance / 30;
-console.log(`The amount of gallons needed if travelling at an average of 55 is: ${numberGallonsNeededIfTravel55Mph}`);
+
 
 // Calculate the amount of gallons needed if travelling at an average of 60 mph
 const numberGallonsNeededIfTravel60Mph = totalDistance / 28;
-console.log(`The amount of gallons needed if travelling at an average of 60 is: ${numberGallonsNeededIfTravel60Mph}`);
+
 
 // Calculate the amount of gallons needed if travelling at an average of 75 mph
 const numberGallonsNeededIfTravel75Mph = totalDistance / 23;
-console.log(`The amount of gallons needed if travelling at an average of 55 is: ${numberGallonsNeededIfTravel75Mph}`);
+console.log(`>>>>>>>>PART 2: PRACTICAL MATH<<<<<<<<
+AMOUNT OF GALLONS NEEDED:
+The amount of gallons needed if travelling at an average of 55 is: ${numberGallonsNeededIfTravel55Mph}. The amount of gallons needed if travelling at an average of 60 is: ${numberGallonsNeededIfTravel60Mph}. The amount of gallons needed if travelling at an average of 75 is: ${numberGallonsNeededIfTravel75Mph}\n`);
 
 
 // COST OF FUEL
 // Calculate the cost of fuel if travelling at an average of 55 mph
 const costIfTravel55Mph = numberGallonsNeededIfTravel55Mph * pricePerGal;
-console.log(`The cost of fuel if I travel at an average of 55 mph is "\$${costIfTravel55Mph}".`);
+
 
 // Calculate the cost of fuel if travelling at an average of 60 mph
 const costIfTravel60Mph = numberGallonsNeededIfTravel60Mph * pricePerGal;
-console.log(`The cost of fuel if I travel at an average of 60 mph is "\$${costIfTravel60Mph}".`);
+
 
 // Calculate the cost of fuel if travelling at an average of 75 mph
 const costIfTravel75Mph = numberGallonsNeededIfTravel75Mph * pricePerGal;
-console.log(`The cost of fuel if I travel at an average of 75 mph is "\$${costIfTravel75Mph}".`);
+console.log(`COST OF FUEL:
+The cost of fuel if I travel at an average of 55 mph is "\$${costIfTravel55Mph}". The cost of fuel if I travel at an average of 60 mph is "\$${costIfTravel60Mph}". The cost of fuel if I travel at an average of 75 mph is "\$${costIfTravel75Mph}". 
+
+Considering the data above, we can conclude that my budget would not be enough if I travel at an average of 75 mph, the most fuel efficient speed, therefore the cheapest one, is 55 mph, and the fastest speed I can go without spending more than /$175 is 60 mph\n`);
+
+
+//DURATION OF THE TRIP:
+// Travelling at 55 mph
+const durationTrip55Mph = (totalDistance / 55).toFixed(2);
+// Travelling at 60 mph
+const durationTrip60Mph = totalDistance / 60;
+// Travelling at 75 mph
+const durationTrip75Mph = totalDistance / 75;
+
+console.log(`DURATION OF THE TRIP:
+- At 55 mph: ${durationTrip55Mph} hrs.
+- At 60 mph: ${durationTrip60Mph} hrs.
+- At 75 mph: ${durationTrip75Mph} hrs.`);
+
